c1bec2f3ee697627aa2053359d528971a21f9460
RyanPintoPassos <119443198+RyanPintoPassos@users.noreply.github.com>
2023-08-23 14:52:53 -0300

    main.js

---
+++ main.js
@@ -0,0 +1,98 @@
+song1 = "";
+song2 = "";
+
+song1_status = "";
+song2_status = "";
+
+scoreTightWrist = 0;
+scoreLeftWrist = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
+leftWristX = 0;
+leftWristY = 0;
+
+function preload()
+{
+    song1 = loadSound("music.mp3");
+    song2 = loadSound("music2.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(600,500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses() 
+{
+    if(SpeechRecognitionResultList.length > 0)
+    {
+        console.log(results);
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreRightWristX = " + scoreRightWristX +" scoreLeftY = "+ leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX +" leftWristy = "+ leftWristY);
+
+    }
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500)
+
+    song1_status = song1.isPlaying();
+    song2_status = song2.isPlaying();
+
+    fill("FF0000");
+    stroke("FF0000");
+
+    if(scoreRightWrist > 0.2)
+    {
+        circle(rightWristX,rightWristY,20);
+
+        song2.stop();
+
+        if(song1_status == false)
+        {
+            song.play();
+            document.getElementById("song").innerHTML = "Tocando: Tema de Harry Potter"
+        }
+    }
+
+    if(scoreLeftWrist > 0.2)
+    {
+        circle(leftWristX,leftWristY,20);
+
+        song1.stop();
+
+        if(song2_status == false)
+        {
+            song2.play();
+            document.getElementById("song").innerHTML = "Tocando: Música de Peter Pan"
+        }
+    }
+}
+
+function play()
+{
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
